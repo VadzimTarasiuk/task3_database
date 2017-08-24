6379f552b08e56e790ac999d397970d11765a015
Vadzim Tarasiuk <vad.tarasyuk@gmail.com>
2017-08-24 09:39:54 +0300

    Tests fixed

--- task3_database/test/smoke/default/default_test.rb
+++ task3_database/test/smoke/default/default_test.rb
@@ -5,14 +5,16 @@
 # The Inspec reference, with examples and extensive documentation, can be
 # found at http://inspec.io/docs/reference/resources/
 
-unless os.windows?
-  # This is an example test, replace with your own test.
-  describe user('root'), :skip do
-    it { should exist }
-  end
+
+describe service('mysql-default') do
+  it { should be_enabled }
+  it { should be_running }
 end
 
-# This is an example test, replace it with your own test.
-describe port(80), :skip do
-  it { should_not be_listening }
+describe port(3306) do
+  it { should be_listening }
+end
+
+describe command('mysql -S /var/run/mysql-default/mysqld.sock -uroot -pvagrant -e "show databases"') do
+  its('stdout') { should match /task3/ }
 end
